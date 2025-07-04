9fd67e05e45339a679b8c8b37389f2eefcf84565
tabithahanegan <naylahanegan@gmail.com>
2025-07-04 15:33:09 -0400

    items appear

--- src/main/java/com/furtabs/asylumresources/AsylumRes.java
+++ src/main/java/com/furtabs/asylumresources/AsylumRes.java
@@ -9,6 +9,7 @@ import com.furtabs.asylumresources.worldgen.ModConfiguredFeatures;
 import com.furtabs.asylumresources.worldgen.ModRegistryEvents;
 import org.slf4j.Logger;
 import net.neoforged.fml.common.Mod;
+import net.neoforged.bus.api.IEventBus;
 import net.minecraft.core.registries.Registries;
 import net.neoforged.neoforge.registries.RegisterEvent;
 import net.neoforged.neoforge.common.NeoForge;
@@ -18,7 +19,16 @@ public class AsylumRes {
     public static final String MOD_ID = "asylumresources";
     private static final Logger LOGGER = LogUtils.getLogger();
 
-    public AsylumRes() {
-        // No registry event registration here; handled by @SubscribeEvent in ModRegistryEvents
+    public AsylumRes(IEventBus modEventBus) {
+        // Register items
+        ModItems.ITEMS.register(modEventBus);
+        
+        // Register blocks
+        ModBlocks.BLOCKS.register(modEventBus);
+        
+        // Register creative tabs
+        ModCreativeModTabs.TABS.register(modEventBus);
+        
+        LOGGER.info("Asylum Resources mod initialized!");
     }
 }
